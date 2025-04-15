fccd06cdd546612e7f52a0aadadc558a0f2f7234
Sara Beatriz Bento <167379586+SaraBeatris@users.noreply.github.com>
2025-04-15 13:04:32 -0300

    Update main.py

--- AvaliaçãoPY/main.py
+++ AvaliaçãoPY/main.py
@@ -4,27 +4,36 @@ from api import API_Rick_Morty, API_Star_Wars
 
 
 class ETL:
-    def consume(self, id, consumer=API_Pokemon() ):
-        if issubclass(type(consumer), API_consumer):
+    def consume(self, id, consumer=API_Pokemon()):
+        if isinstance(consumer, API_consumer):
             print(consumer.extract(id))
         else:
-            print('Falhou')
+            print('Falhou: consumidor inválido')
 
-etl = ETL()
-print('\n\nConsumo da API Pokemon')
-print(40 * '*')
-etl.consume(1, API_Pokemon())
-etl.consume(2)
 
-print('\n\nConsumo da API Rick and Morty')
-print(40 * '*')
-etl.consume(1, API_Rick_Morty())
-etl.consume(2, API_Rick_Morty())
-print('\n\nConsumo da API Star Wars')
-print(40 * '*')
-etl.consume(1, API_Star_Wars())
-etl.consume(2, API_Star_Wars())
-print('\n\nConsumo da API Crônicas do Gelo e do Fogo')
-print(40 * '*')
-etl.consume(583, API_Ice_and_Fire())
-etl.consume(2, API_Ice_and_Fire())
+def main():
+    etl = ETL()
+
+    print('\n\nConsumo da API Pokemon')
+    print(40 * '*')
+    etl.consume(1, API_Pokemon())
+    etl.consume(2)  
+
+    print('\n\nConsumo da API Rick and Morty')
+    print(40 * '*')
+    etl.consume(1, API_Rick_Morty())
+    etl.consume(2, API_Rick_Morty())
+
+    print('\n\nConsumo da API Star Wars')
+    print(40 * '*')
+    etl.consume(1, API_Star_Wars())
+    etl.consume(2, API_Star_Wars())
+
+    print('\n\nConsumo da API Crônicas do Gelo e do Fogo')
+    print(40 * '*')
+    etl.consume(583, API_Ice_and_Fire())
+    etl.consume(2, API_Ice_and_Fire())
+
+
+if __name__ == "__main__":
+    main()
